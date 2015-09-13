117452e33c8fcc4dd26cbd2549aa0ef18397881a
HenriqueLimas <henrique.ramos.limas@gmail.com>
2015-09-13 19:54:59 -0300

    chore(Server): add node server.

---
+++ server.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/comments.json', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
+    });
+  });
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log('First react example, server listening at http://%s:%s', host, port);
+});
